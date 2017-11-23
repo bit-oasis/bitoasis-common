5fc8845f792a4e649779e71b6c705b4ee2725342
Daniel Robenek <daniel.robenek@me.com>
2017-11-23 16:22:24 +0100

    Initial commit

---
+++ src/BitOasis/Common/Console/ConsoleUtils.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace BitOasis\Common\Console;
+
+use BitOasis\Bitfinex\Constant\Symbol;
+use InvalidArgumentException;
+
+/**
+ * @author David Fiedor <davefu@seznam.cz>
+ */
+class ConsoleUtils {
+	
+	const SHORTCUT = 'shortcut';
+	const DESCRIPTION = 'description';
+	const PAIR = 'pair';
+	const SYMBOL = 'symbol';
+	
+	/** @var string[] */
+	protected static $consoleOptions = [
+		'btc' => [
+			self::SHORTCUT => 'b',
+			self::DESCRIPTION => 'BTC',
+			self::PAIR => 'BTC-AED',
+			self::SYMBOL => Symbol::TBTCUSD,
+		],
+		'eth' => [
+			self::SHORTCUT => 'e',
+			self::DESCRIPTION => 'ETH',
+			self::PAIR => 'ETH-AED',
+			self::SYMBOL => Symbol::TETHUSD,
+		],
+		'xrp' => [
+			self::SHORTCUT => 'x',
+			self::DESCRIPTION => 'XRP',
+			self::PAIR => 'XRP-AED',
+			self::SYMBOL => Symbol::TXRPUSD,
+		],
+	];
+
+	private function __construct() {
+	}
+
+	/**
+	 * @return array [option => [shortcut, description, pair, symbol]]
+	 */
+	public function getFullConsoleOptions(): array {
+		return static::$consoleOptions;
+	}
+
+	/**
+	 * @return string[]
+	 */
+	public static function getConsoleOptions(): array {
+		return array_keys(static::$consoleOptions);
+	}
+
+	/**
+	 * @param string $option
+	 * @return string
+	 * @throws InvalidArgumentException
+	 */
+	public static function getOptionShortcut(string $option): string {
+		static::validateOption($option);
+		return static::$consoleOptions[$option][self::SHORTCUT];
+	}
+
+	/**
+	 * @param string $option
+	 * @return string
+	 * @throws InvalidArgumentException
+	 */
+	public static function getOptionDescription(string $option): string {
+		static::validateOption($option);
+		return static::$consoleOptions[$option][self::DESCRIPTION];
+	}
+
+	/**
+	 * @param string $option
+	 * @return string
+	 * @throws InvalidArgumentException
+	 */
+	public static function getOptionPair(string $option): string {
+		static::validateOption($option);
+		return static::$consoleOptions[$option][self::PAIR];
+	}
+
+	/**
+	 * @param string $option
+	 * @return string
+	 * @throws InvalidArgumentException
+	 */
+	public static function getOptionSymbol(string $option): string {
+		static::validateOption($option);
+		return static::$consoleOptions[$option][self::SYMBOL];
+	}
+
+	/**
+	 * @param string $option
+	 * @throws InvalidArgumentException
+	 */
+	protected static function validateOption(string $option) {
+		if (!isset(static::$consoleOptions[$option])) {
+			throw new InvalidArgumentException("Option '$option' doesn't exist!");
+		}
+	}
+
+}
