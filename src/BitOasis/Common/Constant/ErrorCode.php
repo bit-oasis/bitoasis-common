6ac4d5ff97edf9a43242019e08cea398c23efb54
David Fiedor <davefu@seznam.cz>
2018-01-25 17:42:19 +0100

    Added constans for error codes.

---
+++ src/BitOasis/Common/Constant/ErrorCode.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace BitOasis\Common\Constant;
+
+/**
+ * @author David Fiedor <davefu@seznam.cz>
+ */
+class ErrorCode {
+
+	const CONNECTION_ERROR = 1000;
+	const OPERATION_FAILED = 1001;
+	const INVALID_INPUT = 1002;
+
+}
